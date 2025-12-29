f7c662e833a1821423b0434509c9e3d9c7bf2780
DavidPopovich3261 <8534397@gmail.com>
2025-12-29 13:28:51 +0200

    basicauthsystem

--- supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'jsr:@supabase/supabase-js@2'
-import 'dotenv/config'
-
-export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY)
-
-
